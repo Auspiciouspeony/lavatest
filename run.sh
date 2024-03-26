551c38aaa2cccb14214831a5be6f30e2e3324751
Zephyr <82082515+Auspiciouspeony@users.noreply.github.com>
2024-03-26 15:19:03 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://RPCETHLAVA/)
+curl_output=$(curl -H "Content-type: application/json" -X POST --data '{"jsonrpc":"2.0","method":"eth_getBlockByNumber","params":["latest", false],"id":1}' https://eth1.lava.build/lava-referer-be45f41b-9216-4009-850a-1098a22a23da/)
 
 
 echo "$curl_output" >> Update.tsx
